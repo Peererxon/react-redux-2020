c828fc3f4c21bf9376f4d27c79c8f34719ed9014
anderson <andersongil.dev@gmail.com>
2020-11-23 12:04:50 +0100

    agregando action y comenzando a trabajar con el estado

---
+++ src/conmponents/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer(){
+    return (
+        <div>
+            <h2>Numeber of cakes</h2>
+            <button>Cake button</button>
+        </div>
+    )
+}
+
+export default CakeContainer; 
---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    numOfCakes : 10
+}
+
+//inicializamos el estado conun vaor inicial
+const cakeReducer = ( state = initialState,action ) => {
+    switch (action.type) {
+        case 'BUY_CAKE':
+            
+            break;
+    
+        default:
+            break;
+    }
+}
